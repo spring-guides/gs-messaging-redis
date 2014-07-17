cca3d99652f29255fd82a2f5776b74de631d36fe
Thomas Darimont <tdarimont@gopivotal.com>
2014-07-17 20:09:52 +0200

    #6 - Make use of Boot's Jedis-based Redis auto-configuration.
    Applied Andys PR with some minor edits (formatting).

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,66 +1,69 @@
 package hello;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
-import java.util.concurrent.CountDownLatch;
-
 @Configuration
+@EnableAutoConfiguration
 public class Application {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
-    @Bean
-    JedisConnectionFactory connectionFactory() {
-        return new JedisConnectionFactory();
-    }
-    
-    @Bean
-    RedisMessageListenerContainer container(JedisConnectionFactory connectionFactory,
-                                            MessageListenerAdapter listenerAdapter) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
-        return container;
-    }
-    
-    @Bean
-    MessageListenerAdapter listenerAdapter(Receiver receiver) {
-        return new MessageListenerAdapter(receiver, "receiveMessage");
-    }
+	@Bean
+	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+			MessageListenerAdapter listenerAdapter) {
 
-    @Bean
-    Receiver receiver(CountDownLatch latch) {
-        return new Receiver(latch);
-    }
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
 
-    @Bean
-    CountDownLatch latch() {
-        return new CountDownLatch(1);
-    }
-    
-    @Bean
-    StringRedisTemplate template(JedisConnectionFactory connectionFactory) {
-        return new StringRedisTemplate(connectionFactory);
-    }
-    
-    public static void main(String[] args) throws InterruptedException {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
-        CountDownLatch latch = ctx.getBean(CountDownLatch.class);
-        LOGGER.info("Sending message...");
-        template.convertAndSend("chat", "Hello from Redis!");
-        latch.await();
-        System.exit(0);
-    }
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	@Bean
+	Receiver receiver(CountDownLatch latch) {
+		return new Receiver(latch);
+	}
+
+	@Bean
+	CountDownLatch latch() {
+		return new CountDownLatch(1);
+	}
+
+	@Bean
+	StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+		return new StringRedisTemplate(connectionFactory);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+		StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
+		CountDownLatch latch = ctx.getBean(CountDownLatch.class);
+
+		LOGGER.info("Sending message...");
+		template.convertAndSend("chat", "Hello from Redis!");
+
+		latch.await();
+
+		System.exit(0);
+	}
 }
