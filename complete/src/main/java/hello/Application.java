1a8b65a918bd4cfd43be16138e6ec9edc68b0829
Thomas Darimont <tdarimont@pivotal.io>
2015-02-23 16:52:52 +0100

    Use @SpringBootApplication to enable Spring Boot configuration.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -5,18 +5,16 @@ import java.util.concurrent.CountDownLatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
