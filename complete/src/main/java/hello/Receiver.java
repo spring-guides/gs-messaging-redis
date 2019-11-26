2b7c4793ad07930f2c15931c53147111f3255b7b
Greg Turnquist <gturnquist@pivotal.io>
2019-11-26 13:08:44 -0600

    Switch to tabs, and adjust tabsizing in asciidoctor.

--- complete/src/main/java/hello/Receiver.java
+++ complete/src/main/java/hello/Receiver.java
@@ -7,17 +7,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class Receiver {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
 
-    private CountDownLatch latch;
+	private CountDownLatch latch;
 
-    @Autowired
-    public Receiver(CountDownLatch latch) {
-        this.latch = latch;
-    }
+	@Autowired
+	public Receiver(CountDownLatch latch) {
+		this.latch = latch;
+	}
 
-    public void receiveMessage(String message) {
-        LOGGER.info("Received <" + message + ">");
-        latch.countDown();
-    }
+	public void receiveMessage(String message) {
+		LOGGER.info("Received <" + message + ">");
+		latch.countDown();
+	}
 }
